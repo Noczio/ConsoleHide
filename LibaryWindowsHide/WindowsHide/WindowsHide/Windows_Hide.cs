ac1c53fa60b0fde0dfae6b5f0e0e28822ceb2606
Dev Nocz <54759141+Noczio@users.noreply.github.com>
2019-11-09 15:24:12 -0500

    Files added
    Project files added

---
+++ LibaryWindowsHide/WindowsHide/WindowsHide/Windows_Hide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ExtLibraries
+{
+    public static class Windows_Hide
+    {
+        #region SubMethods
+        // The two dll imports below will handle the window hiding.
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+
+        #endregion
+
+        #region Methods
+        public static void Hide_Console()
+        {
+            var handle = GetConsoleWindow();                
+            ShowWindow(handle, SW_HIDE);
+        }
+        #endregion
+    }
+}
